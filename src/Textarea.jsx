7c328357625ab669e46bd990d0726027d32b2592
shivam <shivamm0383@gmail.com>
2024-11-11 17:57:33 +0530

    textarea-component-completed

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,17 +1,34 @@
 import { useState } from "react";
+import Warning from "./Warning";
 
 export default function Textarea() {
   const [text, setText] = useState("");
+  const [warningText, setWarningText] = useState("");
+  const handleChange = (e) => {
+    let newText = e.target.value;
+    //basic validationss
+
+    if (newText.includes("<script>")) {
+      setWarningText("No script tag allowed");
+      newText = newText.replace("<script>", "");
+    } else if (newText.includes("@")) {
+      setWarningText("No @ tag allowed");
+      newText = newText.replace("@", "");
+    } else {
+      setWarningText("");
+    }
+    setText(newText);
+  };
+
   return (
-    <textarea
-      value={text}
-      onChange={(e) => {
-        const newText = e.target.value;
-        setText(newText);
-      }}
-      className="textarea"
-      placeholder="Enter Your Text "
-      spellCheck="false"
-    />
+    <div className="textarea">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter Your Text "
+        spellCheck="false"
+      />
+      <Warning warningText={warningText} />
+    </div>
   );
 }
