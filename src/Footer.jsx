f420f33f480003d64a1caac4f6fe837d0d0d1691
shivam <shivamm0383@gmail.com>
2024-11-11 17:33:05 +0530

    css-added&different-component-created

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>&copy; Copyright by Shivam. All rights reserved</small>
+      <small>last checked limits: 10 days ago</small>
+    </footer>
+  );
+}
---
+++ src/Textarea.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export default function Textarea() {
+  const [text, setText] = useState("");
+  return (
+    <textarea
+      value={text}
+      onChange={(e) => {
+        const newText = e.target.value;
+        setText(newText);
+      }}
+      className="textarea"
+      placeholder="Enter Your Text "
+      spellCheck="false"
+    />
+  );
+}
